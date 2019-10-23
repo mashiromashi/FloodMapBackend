8d9478f2ace40479228884a3658819ea6d2fa69a
Htin Wana <Htinwana.dev@gmail.com>
2019-10-23 16:07:26 +0630

    added month array

--- routes/route.laboSensor.js
+++ routes/route.laboSensor.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const laboModel = require("../models/model.laboSensor");
 const app = express();
+const monthArray = require("../util/months");
+const moment = require("moment");
 
 app.get("/getall", async (req, res) => {
   const labo = await laboModel.find({});
@@ -20,11 +22,30 @@ app.get("/getlatest", async (req, res) => {
     res.status(500).send(err);
   }
 });
+
+// //query for monthly statistics
+// app.get("/monthly?q=:inputMonth", async (req, res) => {
+//   let currentMomentMonth = moment().month();
+//   let currentMonth = monthArray[currentMomentMonth];
+
+//   const inputMonth = req.params.input
+//   const monthly = await laboModel.find({
+//     $text: {
+//       $search: inputMonth
+//     }
+//   });
+//   if (monthly) {
+//     try {
+//       res.status(200).send
+//     }
+//   }
+// });
+
 app.post("/insert", async (req, res) => {
   const labo = new laboModel(req.body);
   try {
     await labo.save();
-    res.send(labo);
+    res.status(201).send(labo);
   } catch (err) {
     res.status(500).send(err);
   }
